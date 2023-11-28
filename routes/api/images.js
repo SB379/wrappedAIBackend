02a43298fa7890ac41fd2382db786405909762cb
SB379 <sidbalaji03@gmail.com>
2023-11-27 23:25:25 -0500

    please for the love of god stop with the import issues

--- routes/api/images.js
+++ routes/api/images.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 require('dotenv').config();
 const axios = require('axios');
-const fs = require('fs');
 
 const { OpenAI } = require("openai");
 
